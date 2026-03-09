e37ce9608af68c900f8cb964285464152dedcd21
Dung Pham <dungpham197qn@gmail.com>
2026-03-09 10:09:55 +0700

    first commit

---
+++  HN_KS23_Pham Quang Dung_009/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main style={{ fontFamily: 'Arial, sans-serif', padding: 24 }}>
+      <h1>Rikkei Fullstack Sample</h1>
+      <p>Frontend is running.</p>
+    </main>
+  );
+}
+
+export default App;
